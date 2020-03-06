b515af870493e087d1e05760e945f8feeb30b8c7
kuelye <kuelye@gmail.com>
2020-03-06 15:06:23 +0300

    II.15

--- app/src/main/java/com/banana/b19/promo/MainActivity.kt
+++ app/src/main/java/com/banana/b19/promo/MainActivity.kt
@@ -2,10 +2,13 @@ package com.banana.b19.promo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +16,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         viewPager.adapter = Adapter() // присоединяем к ViewPager адаптер
+        viewPager.setPageTransformer(true, ZoomTransformer())
         tabLayout.setupWithViewPager(viewPager) // подключаем ViewPager к TabLayout
+        circleIndicator.setViewPager(viewPager) // подключаем ViewPager к CircleIndicator
+    }
+
+    class ZoomTransformer : ViewPager.PageTransformer {
+
+        override fun transformPage(page: View, position: Float) {
+            var scale = 1 - position.absoluteValue
+            if (scale < .8f) scale = .8f
+            page.scaleX = scale
+            page.scaleY = scale
+            page.alpha = scale
+            page.rotation = 180 * position
+        }
+
     }
 
     /* адаптер на основе входных данных (число страниц и так далее) создаёт фрагменты */
@@ -27,6 +45,13 @@ class MainActivity : AppCompatActivity() {
                 0 -> PageFragment()
                 1 -> PageFragment()
                 else -> PageFragment()
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return when (position) {
+                0 -> "A"
+                else -> position.toString()
             }
         }
 
